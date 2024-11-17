4a85f3f4e185fb7ef0a4f664f317438f5a060e7b
Vikranth Kanumuru <saivicky2015@gmail.com>
2024-11-17 04:11:54 -0800

    Commit with firecrawl and with together

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from firecrawl import FirecrawlApp
+from together import Together
 import time
 
 # Load environment variables
@@ -27,27 +28,42 @@ def get_crawler():
     """
     return FirecrawlApp(api_key=app.config['FIRECRAWL_SECRET_KEY'])
 
+
+def get_together():
+    """
+    Factory function to get together instance
+    """
+    return Together(api_key=app.config['TOGETHER_SECRET_KEY'])
+
 # Initialize FireCrawler (placeholder)
 crawler = get_crawler()
-
+together = get_together()
 @app.route('/echo', methods=['POST'])
 def echo():
     """Simple echo endpoint to test the API"""
     data = request.get_json()
     return jsonify({"message": data.get('text', '')})
 
-@app.route('/crawl', methods=['POST'])
-def crawl():
+@app.route('/findpeople', methods=['POST'])
+def findpeople():
     """Endpoint to crawl websites using firecrawl"""
     data = request.get_json()
-    url = data.get('url')
-    
-    if not url:
-        return jsonify({"error": "URL is required"}), 400
+    # url = data.get('url')
+    requirements = data.get('requirements')
+    # if not url:
+    #     return jsonify({"error": "URL is required"}), 400
     
     try:
-        result = crawler.scrape_url(url, params={'formats': ['markdown', 'html']})
-        # ADD this to together API and send back the result that your gonna get to client
+        # result = crawler.scrape_url(url, params={'formats': ['markdown']})
+        result = crawler.scrape_url("https://www.carelinx.com/caregivers/ca/sunnyvale", params={'formats': ['markdown']})
+
+        # Send the result to the Together API, with a system prompt to summarize the content
+        system_prompt = "You are given a list of people and a list of requirements that your looking for, based on the second input that your getting, I want you to match the people that match the input that you are being given in the list of people. The output should be a list of people that match the requirements."
+        together_response = together.chat.completions.create(
+            model="meta-llama/Meta-Llama-3-70B-Instruct-Turbo",
+            messages=[{"role": "system", "content": system_prompt},  {"role": "user", "content": result}]
+        )
+        
 
         return jsonify(result)
     except Exception as e:
